2ba58278e3f62973d0cce4ea1cb3d35b486d68f4
12 年二 <2812387227@qq.com>
2025-04-18 19:43:03 +0800

    the third commit for the cpp homework

--- cpp_homework/source_code.cpp
+++ cpp_homework/source_code.cpp
@@ -2,6 +2,7 @@
 #include<graphics.h>
 #include<string>
 #include<vector>
+#include<cmath>
 
 
 IMAGE img_background;
@@ -9,6 +10,10 @@ IMAGE img_shadow;
 
 //绘制透明底图片的自定义函数
 #pragma comment(lib,"MSIMG32.LIB")
+#pragma comment(lib,"Winmm.lib")
+
+bool running = true;
+bool is_game_started = false;
 
 inline void putimage_alpha(int x, int y, IMAGE* img) {
 	int w = img->getwidth();
@@ -18,11 +23,9 @@ inline void putimage_alpha(int x, int y, IMAGE* img) {
 }
 
 
-class Animation {
+class Atlas {
 public:
-	Animation(LPCTSTR path, int num, int interval) {
-		interval_ms = interval;
-
+	Atlas(LPCTSTR path, int num) {
 		TCHAR path_file[256];
 		for (size_t i = 0; i < num; i++) {
 			_stprintf_s(path_file, path, i);
@@ -31,25 +34,42 @@ public:
 			loadimage(frame, path_file);
 			frame_list.push_back(frame);//利用pushback函数把图片对象的指针添加到容器里面
 		}
-
 	}
-
-	~Animation() {
+	~Atlas() {
 		for (size_t i = 0; i < frame_list.size(); i++) {
 			delete frame_list[i];
 		}
 	}
+
+public:
+	std::vector<IMAGE*>frame_list;
+};
+	
+Atlas* atlas_player_left;
+Atlas* atlas_player_right;
+Atlas* atlas_enemy_left;
+Atlas* atlas_enemy_right;
+
+
+class Animation {
+public:
+	Animation(Atlas* atlas, int interval) {
+		anim_atlas = atlas;
+		interval_ms = interval;
+	}
+
+	~Animation() = default;
 
 	//动画播放
 	void Play(int x, int y, int delta) {
 		timer += delta;
 
 		if (timer >= interval_ms) {
-			idx_frame = (idx_frame + 1) % frame_list.size();
+			idx_frame = (idx_frame + 1) % anim_atlas->frame_list.size();
 			timer = 0;
 		}
 
-		putimage_alpha(x, y, frame_list[idx_frame]);
+		putimage_alpha(x, y,anim_atlas-> frame_list[idx_frame]);
 	}
 
 
@@ -57,7 +77,7 @@ private:
 	int timer = 0;//动画计时器
 	int idx_frame = 0;//动画帧索引
 	int interval_ms = 0;
-	std::vector<IMAGE*>frame_list;
+	Atlas* anim_atlas;
 };
 
 
@@ -66,15 +86,22 @@ private:
 const int WIDTH0 = 1280;//页面宽度
 const int HEIGHT0 = 720;//页面高度
 
+const int BUTTON_WIDTH = 192;
+const int BUTTON_HEIGHT = 75;
+
+
+
+
 class Player {
 public:
 	const int PLAYER_WIDTH = 80;//玩家宽度
 	const int PLAYER_HEIGHT = 80;//玩家高度
+	POINT position = { 300,300 };//初始化玩家位置
 public:
 	Player() {
 		loadimage(&img_shadow, _T("img/shadow_player.png"));
-		anim_left = new Animation(_T("img/player_left_%d.png"), 6, 45);
-		anim_right = new Animation(_T("img/player_right_%d.png"), 6, 45);
+		anim_left = new Animation(atlas_player_left, 45);
+		anim_right = new Animation(atlas_player_right, 45);
 	};
 
 	~Player() {
@@ -82,10 +109,8 @@ public:
 		delete anim_right;
 	}
 
-	void ProcessEvent(ExMessage& msg) {
-		while (peekmessage(&msg)) {
-			//读取操作
-
+	
+	void ProcessEvent(const ExMessage& msg) {
 			//通过按键来实现角色移动
 			if (msg.message == WM_KEYDOWN) {
 				switch (msg.vkcode) {
@@ -119,8 +144,6 @@ public:
 					break;
 				}
 			}
-		}
-
 	}
 
 	void Move() {
@@ -172,11 +195,9 @@ public:
 
 
 private:
-	const int PLAYER_WIDTH = 80;//玩家宽度
-	const int PLAYER_HEIGHT = 80;//玩家高度
 	const int SHADOW_WIDTH = 32;//影子大小
-	const int SPEED = 3;//定义速度
-	POINT position = { 300,300 };//初始化玩家位置
+	const int SPEED = 5;//定义速度
+	
 
 private:
 	IMAGE img_shadow;
@@ -191,22 +212,44 @@ private:
 
 
 
+class Bullet {
+public:
+	POINT position = { 0,0 };
+
+public:
+	Bullet() = default;
+	~Bullet() = default;
+
+	void Draw() const {
+		setlinecolor(RED);
+		setfillcolor(RED);
+		fillcircle(position.x, position.y, R);
+	}
+
+	int GetR()const {
+		return R;
+	}
+private:
+	const int R = 8;
+};
+
+
 
 class Enemy {
 public:
+	//敌人生成边界
+	enum class SpawnEdge {
+		Up = 0,
+		Down,
+		Left,
+		Right
+
+	};
+
 	Enemy() {
 		loadimage(&img_shadow, _T("img/shadow_enemy.png"));
-		anim_left = new Animation(_T("img/enemy_left_%d.png"), 6, 45);
-		anim_right = new Animation(_T("img/enemy_right_%d.png"), 6, 45);
-
-		//敌人生成边界
-		enum class SpawnEdge {
-			Up = 0,
-			Down,
-			Left,
-			Right
-
-		};
+		anim_left = new Animation(atlas_enemy_left,45);
+		anim_right = new Animation(atlas_enemy_right, 45);
 
 		SpawnEdge edge = (SpawnEdge)(rand() % 4);
 		switch (edge) {
@@ -232,18 +275,24 @@ public:
 	}
 
 	bool CheckBulletCollision(const Bullet& bullet) {
-		return false;
+		//将子弹等效为点，判断是否在敌人矩形内
+		bool is_overlap_x = bullet.position.x >= position.x && bullet.position.x <= position.x + FRAME_WIDTH;
+		bool is_overlap_y = bullet.position.y >= position.y && bullet.position.y <= position.y + FRAME_HEIGHT;
+		return is_overlap_x && is_overlap_y;
 	}
 
 	bool CheckPlayerCollision(const Player& player) {
 		//将敌人中心位置等效为点，判断是否在玩家矩形内
 		POINT check_position = { position.x + FRAME_WIDTH / 2,position.y + FRAME_HEIGHT / 2 };
-		return false;
+		bool is_overlap_x = player.position.x >= position.x && player.position.x <= position.x + FRAME_WIDTH;
+		bool is_overlap_y = player.position.y >= position.y && player.position.y <= position.y + FRAME_HEIGHT;
+		return is_overlap_x && is_overlap_y;
 	}
 
 	void Move(const Player& player) {
 		const POINT& player_position = player.GetPosition();
 		int dir_x = player_position.x - position.x;
+		facing_left = (dir_x < 0);
 		int dir_y = player_position.y - position.y;
 		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);
 		if (len_dir != 0) {
@@ -256,7 +305,7 @@ public:
 
 	void Draw(int delta) {
 		int pos_shadow_x = position.x + (FRAME_WIDTH / 2 - SHADOW_WIDTH / 2);
-		int pos_shadow_y = position.y + FRAME_HEIGHT - 8;//偏移一点点
+		int pos_shadow_y = position.y + FRAME_HEIGHT - 31;//偏移一点点
 		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
 
 
@@ -274,11 +323,21 @@ public:
 		delete anim_left;
 		delete anim_right;
 	}
+
+	void Hurt() {
+		alive = false;
+	}
+
+	bool CheckAlive() {
+		return alive;
+	}
+
+
 private:
 	const int FRAME_WIDTH = 80;//敌人宽度
 	const int FRAME_HEIGHT = 80;//敌人高度
 	const int SHADOW_WIDTH = 48;//影子大小
-	const int SPEED = 2;//定义速度
+	const int SPEED = 5;//定义速度
 
 private:
 	IMAGE img_shadow;
@@ -286,6 +345,7 @@ private:
 	Animation* anim_right;
 	POINT position = { 0,0 };//初始化敌人位置};
 	bool facing_left = false; 
+	bool alive = true;
 	bool is_move_up = false;
 	bool is_move_down = false;
 	bool is_move_left = false;
@@ -295,22 +355,103 @@ private:
 
 
 
-class Bullet {
+class Button {
 public:
-	POINT position = { 0,0 };
+	Button(RECT rect,LPCTSTR path_img_idle,LPCTSTR path_img_hovered,LPCTSTR path_img_pushed) {
+		region = rect;
+		loadimage(&img_idle, path_img_idle);
+		loadimage(&img_hovered, path_img_hovered);
+		loadimage(&img_pushed, path_img_pushed);
 
-public:
-	Bullet() = default;
-	~Bullet() = default;
+	}
 
-	void Draw() const {
-		setlinecolor(RED);
-		setfillcolor(RED);
-		fillcircle(position.x, position.y, R);
+	~Button() = default;
+
+	void ProcessEvent(const ExMessage& msg) {
+		switch (msg.message) {
+		case WM_MOUSEMOVE:
+			if (status == Status::Idle && CheckCursorHit(msg.x, msg.y))
+				status = Status::Hovered;
+			else if (status == Status::Hovered && CheckCursorHit(msg.x, msg.y))
+				status = Status::Idle;
+			break;
+		case WM_LBUTTONDOWN:
+			if (CheckCursorHit(msg.x, msg.y))
+				status = Status::Pushed;
+			break;
+		case WM_LBUTTONUP:
+			if (status == Status::Pushed)
+				OnClick();
+			break;
+		default:
+			break;
+		}
+	}
+
+	void Draw() {
+		switch (status) {
+		case Status::Idle:
+			putimage(region.left, region.top, &img_idle);
+			break;
+		case Status::Hovered:
+			putimage(region.left, region.top, &img_hovered);
+			break;
+		case Status::Pushed:
+			putimage(region.left, region.top, &img_pushed);
+			break;
+		}
+	}
+
+protected:
+	virtual void OnClick() = 0;
+
+private:
+	enum class Status {
+		Idle=0,
+		Hovered,
+		Pushed
+	};
+
+private:
+	//检测鼠标点击
+	bool CheckCursorHit(int x, int y) {
+		return x >= region.left && x <= region.right && y >= region.top && y <= region.bottom;
 	}
 
 private:
-	const int R = 10;
+	RECT region;//描述位置和大小
+	IMAGE img_idle;
+	IMAGE img_hovered;
+	IMAGE img_pushed;
+	Status status = Status::Idle;
+};
+
+
+//开始游戏按钮
+class StartGameButton :public Button {
+public:
+	StartGameButton(RECT rect, LPCTSTR path_img_idle, LPCTSTR path_img_hovered, LPCTSTR path_img_pushed)
+		:Button(rect,path_img_idle,path_img_hovered,path_img_pushed){}
+	~StartGameButton() = default;
+
+protected:
+	void OnClick() {
+		is_game_started = true;
+
+		mciSendString(_T("play bgm repeat from 0"), NULL, 0, NULL);
+	}
+};
+//退出游戏按钮
+class QuitGameButton :public Button {
+public:
+	QuitGameButton(RECT rect, LPCTSTR path_img_idle, LPCTSTR path_img_hovered, LPCTSTR path_img_pushed)
+		:Button(rect, path_img_idle, path_img_hovered, path_img_pushed) {}
+	~QuitGameButton() = default;
+
+protected:
+	void OnClick() {
+		running = false;
+	}
 };
 
 
@@ -340,9 +481,6 @@ void LoadAnimation() {
 }
 
 
-Animation anim_left_player(_T("img/player_left_%d.png"), 6, 45);
-Animation anim_right_player(_T("img/player_right_%d.png"), 6, 45);
-
 
 //生成新的敌人
 void TryGenerateEnemy(std::vector<Enemy*>& enemy_list) {
@@ -353,49 +491,159 @@ void TryGenerateEnemy(std::vector<Enemy*>& enemy_list) {
 			}
 };
 
+//更新子弹的位置
+void UpdateBullets(std::vector<Bullet>& bullet_list, const Player& player) {
+	const double R_SPEED = 0.004;
+	const double T_SPEED = 0.004;
+	double radian_interval = 2 * 3.1415926 / bullet_list.size();
+	POINT player_position = player.GetPosition();
+	double radius = 100 + 25 * sin(GetTickCount() * R_SPEED);
+	for (size_t i = 0; i < bullet_list.size(); i++) {
+		double radian = GetTickCount() * T_SPEED + radian_interval * i;
+		bullet_list[i].position.x = player_position.x + player.PLAYER_WIDTH / 2 + (int)(radius * sin(radian));
+		bullet_list[i].position.y = player_position.y + player.PLAYER_HEIGHT / 2 + (int)(radius * cos(radian));
+	}
+}
+
+
+
+void DrawPlayerScore(int score) {
+	static TCHAR text[64];
+	_stprintf_s(text, _T("当前玩家得分为：%d"), score);
+
+	setbkmode(TRANSPARENT);
+	settextcolor(WHITE);
+	outtextxy(10, 10, text);
+}
 
 
 int main() {
 	initgraph(WIDTH0, HEIGHT0);
 
-	bool running = true;
+	atlas_player_left = new Atlas(_T("img/player_left_%d.png"), 6);
+	atlas_player_right = new Atlas(_T("img/player_right_%d.png"), 6);
+	atlas_enemy_left = new Atlas(_T("img/enemy_left_%d.png"), 6);
+	atlas_enemy_right = new Atlas(_T("img/enemy_right_%d.png"), 6);
 
+	mciSendString(_T("open mus/bgm.mp3 alias bgm"), NULL, 0, NULL);
+	mciSendString(_T("open mus/hit.wav alias hit"), NULL, 0, NULL);
+
+
+	int score = 0;
 	Player player;
-
 	ExMessage msg;
-
-	std::vector<Enemy*>enemy_list;
-
+	IMAGE img_menu;
+	IMAGE img_background;
 
 	bool is_move_up = false;
 	bool is_move_down = false;
 	bool is_move_left = false;
 	bool is_move_right = false;
+	std::vector<Enemy*>enemy_list;
+	std::vector<Bullet>bullet_list(3);
 
-	Bullet bullet;
+	RECT region_btn_start_game, region_btn_quit_game;
 
-	LoadAnimation();
+	region_btn_start_game.left = (WIDTH0 - BUTTON_WIDTH) / 2;
+	region_btn_start_game.right = region_btn_start_game.left + BUTTON_WIDTH;
+	region_btn_start_game.top = 430;
+	region_btn_start_game.bottom = region_btn_start_game.top + BUTTON_HEIGHT;
 
+	region_btn_quit_game.left = (WIDTH0 - BUTTON_WIDTH) / 2;
+	region_btn_quit_game.right = region_btn_quit_game.left + BUTTON_WIDTH;
+	region_btn_quit_game.top = 550;
+	region_btn_quit_game.bottom = region_btn_quit_game.top + BUTTON_HEIGHT;
 
+	StartGameButton btn_start_game = StartGameButton(region_btn_start_game,
+		_T("img/ui_start_idle.png"), _T("img/ui_start_hovered.png"), _T("img/ui_start_pushed.png"));
+	QuitGameButton btn_quit_game = QuitGameButton(region_btn_quit_game,
+		_T("img/ui_quit_idle.png"), _T("img/ui_quit_hovered.png"), _T("img/ui_quit_pushed.png"));
+
+	loadimage(&img_menu, _T("img/menu.png"), 1280, 720);
 	loadimage(&img_background, _T("img/background.png"), 1280, 720);
 
 	BeginBatchDraw();
 
 	while (running) {
+
 		DWORD begin_time = GetTickCount();
 
-
+		ExMessage msg;
 		while (peekmessage(&msg)) {
-			player.ProcessEvent(msg);
+			if (is_game_started) {
+				player.ProcessEvent(msg);
+			}
+			else {
+				btn_start_game.ProcessEvent(msg);
+				btn_quit_game.ProcessEvent(msg);
+			}
 		}
 
+		if (is_game_started) {
+			player.Move();
+			UpdateBullets(bullet_list, player);
+			TryGenerateEnemy(enemy_list);
+			//更新敌人位置
+			for (Enemy* enemy : enemy_list)
+				enemy->Move(player);
 
-		player.Move();
-		TryGenerateEnemy(enemy_list);
-		for (Enemy* enemy : enemy_list)
-			enemy->Move(player);
+			//检测子弹与敌人的碰撞
+			for (Enemy* enemy : enemy_list) {
+				for (const Bullet& bullet : bullet_list) {
+					if (enemy->CheckBulletCollision(bullet)) {
+						enemy->Hurt();
+						score++;
+					}
+				}
+			}
+
+			//移除生命值归零的敌人
+			for (size_t i = 0; i < enemy_list.size(); i++) {
+				Enemy* enemy = enemy_list[i];
+				if (!enemy->CheckAlive()) {
+					std::swap(enemy_list[i], enemy_list.back());
+					enemy_list.pop_back();
+					delete enemy;
+				}
+			}
 
 
+			// 检测敌人与玩家的碰撞
+			bool is_game_over = false;  // 增加游戏结束标志
+			for (Enemy* enemy : enemy_list) {
+				if (enemy->CheckPlayerCollision(player)) {
+					is_game_over = true;
+					break;  // 发现碰撞立即跳出循环
+				}
+			}
+
+			if (is_game_over) {
+				// 游戏结束逻辑
+				TCHAR text[128];
+				_stprintf_s(text, _T("最终得分为：%d！\n点击确定返回主菜单"), score);
+
+				// 停止音乐
+				mciSendString(_T("stop bgm"), NULL, 0, NULL);
+
+				// 显示结果弹窗
+				if (MessageBox(GetHWnd(), text, _T("游戏结束"), MB_OKCANCEL | MB_ICONINFORMATION) == IDOK) {
+					// 重置游戏状态
+					is_game_started = false;
+					score = 0;
+					player.position = { 300, 300 };  // 重置玩家位置
+
+					// 清空敌人
+					for (auto& enemy : enemy_list) delete enemy;
+					enemy_list.clear();
+				}
+				else {
+					running = false;  // 如果点取消则退出游戏
+				}
+
+			
+			}
+		}
+		
 		static int counter = 0;
 		if (++counter % 5 == 0) {
 			idx_current_anim++;
@@ -408,21 +656,42 @@ int main() {
 		cleardevice();
 		//绘图
 
-		putimage(0, 0, &img_background);//铺背景图片
+		if (is_game_started) {
+			putimage(0, 0, &img_background);//铺背景图片
 
-		player.Draw(1000 / 144);
-		for (Enemy* enemy : enemy_list)
-			enemy->Draw(1000 / 144);
-
+			player.Draw(1000 / 144);
+			for (Enemy* enemy : enemy_list)
+				enemy->Draw(1000 / 144);
+			for (const Bullet& bullet : bullet_list)
+				bullet.Draw();
+			DrawPlayerScore(score);
+		}
+		else {
+			putimage(0, 0, &img_menu);
+			btn_start_game.Draw();
+			btn_quit_game.Draw();
+		}
+		
 
 		FlushBatchDraw();
 
 		DWORD end_time = GetTickCount();
 		DWORD delta_time = end_time - begin_time;
-
 		if (delta_time < 1000 / 60) {
 			Sleep(1000 / 60 - delta_time);
 		}
+	}
+
+	delete atlas_player_left;
+	delete atlas_player_right;
+	delete atlas_enemy_left;
+	delete atlas_enemy_right;
+
+	EndBatchDraw();
+
+	while (!enemy_list.empty()) {
+		delete enemy_list.back();
+		enemy_list.pop_back();
 	}
 
 	return 0;
